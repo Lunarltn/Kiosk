3f4d7149ff3960c8335f91d7b581f6b7906d83ee
kisu kim <qpsrltn@gmail.com>
2025-07-21 14:20:09 +0900

    [feat] 햄버거 메뉴를 MenuItem 클래스와 List를 통해 관리

---
+++ src/main/java/com/example/kiosk/MenuItem.java
@@ -0,0 +1,28 @@
+package com.example.kiosk;
+
+/**
+ * 세부 메뉴 속성을 가지는 클래스
+ */
+public class MenuItem {
+    private String name;
+    private double price;
+    private String comment;
+
+    public MenuItem(String name, double price, String comment) {
+        this.name = name;
+        this.price = price;
+        this.comment = comment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+}
